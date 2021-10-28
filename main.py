ffc728ee9a4617fffd07c9ad6fa13cd4dd7f22fd
bhupinderdhanda <55067184+bhupinderdhanda@users.noreply.github.com>
2021-10-28 17:36:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from PIL import Image
 
 from pixelmatch.contrib.PIL import pixelmatch
 
+# opening image 1
 img_a = Image.open("a.png")
+
+# opening image 2
 img_b = Image.open("b.png")
+
+# calculating image difference
 img_diff = Image.new("RGBA", img_a.size)
 
 # note how there is no need to specify dimensions
